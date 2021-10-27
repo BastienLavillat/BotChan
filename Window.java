7fcfbd625d6816b7fcf1ce81e83b6188f49cfb0b
Bastien <basvillat.game@gmail.com>
2021-10-27 15:49:12 +0200

    first commit

---
+++ Window.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Window extends JFrame
+{
+    public Window()
+    {
+        super("BotChan");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
