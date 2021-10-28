31458b42abf953ebb79879d924a4e15911d1b412
Bastien <basvillat.game@gmail.com>
2021-10-28 11:39:55 +0200

    first commit

--- BotChan.java
+++ BotChan.java
@@ -1,7 +1,9 @@
 class BotChan
 {
+    private static Window window;
+
     public static void main(String[] args)
     {
-        System.out.println("Hello World !");
+        window = new Window();
     }
 }
--- Window.java
+++ Window.java
@@ -5,6 +5,15 @@ public class Window extends JFrame
     public Window()
     {
         super("BotChan");
+        setSize(500, 500);
+
+        // stop the program when window is closed
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // center the window inside the screen
+        setLocationRelativeTo(null);
+
+        // display the window
+        setVisible(true);
     }
 }
